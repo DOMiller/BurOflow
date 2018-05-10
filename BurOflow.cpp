2c5afb90a6981da64b235e146b866b09fea4729e
David Miller <david.miller@magnetimarelli.com>
2018-05-10 12:50:54 -0400

    Add stuff

---
+++ BurOflow.cpp
@@ -0,0 +1,25 @@
+// BurOflow.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "string.h"
+#include "stdio.h"
+
+int main()
+{
+	int strlength;
+	char instring[16], outstring[16];
+
+	while (1) {
+		printf("\n\nYour test word (nax 15 non-whitespace chars; '.' to stop)?  ");
+		scanf("%15s", instring);
+		rewind(stdin);
+		if (instring[0] == '.') break;
+		sprintf(outstring, instring);
+		strlength = strlen(outstring);
+		if (strlength < 16) printf("\n Your test phrase is:  %s  (%d chars)", outstring, strlength);
+		else printf("\n*** BUFFER OVERFLOW (by %d chars) ***", strlength - 15);
+	}
+	return 0;
+}
+
