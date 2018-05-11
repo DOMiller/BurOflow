7dd73a4cd75979180cee80f684da4af93779c13e
David Miller <david.miller@magnetimarelli.com>
2018-05-11 14:55:00 -0400

    Revert

--- BurOflow.c
+++ BurOflow.c
@@ -9,12 +9,12 @@ int main()
 	char instring[16], outstring[16];
 
 	while (1) {
-		printf("\n\nYour test word (max 15 non-whitespace chars; '^c' to stop)?  ");
+		printf("\n\nYour test word (max 15 non-whitespace chars; '.' to stop)?  ");
 		scanf("%15s", instring);
 		rewind(stdin);
-//		if (instring[0] == '.') break;
-		sprintf(outstring, (char const *)instring);
-		strlength = strlen((char const *)outstring);
+		if (instring[0] == '.') break;
+		sprintf(outstring, instring);
+		strlength = strlen(outstring);
 		if (strlength < 16) printf("\n Your test phrase is:  %s  (%d chars)", outstring, strlength);
 		else printf("\n*** BUFFER OVERFLOW (by %d chars) ***", strlength - 15);
 	}
